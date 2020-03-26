bc914c9e361dad51adc1365b6a8b0c7ade252ac0
ancndz <ytkaev12@live.com>
2020-03-26 14:58:42 +0300

    set new default size

--- src/pack/kursovaya/app/MainFrame.java
+++ src/pack/kursovaya/app/MainFrame.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
-        setSize(800, 600);
+        setSize(900, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(800, 600));
         setLocationRelativeTo(null);
